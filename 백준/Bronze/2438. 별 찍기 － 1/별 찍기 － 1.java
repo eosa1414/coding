61b724ce97d8ff2209292d2fd16951af68e0b5aa
eosa1414 <eosa1414@gmail.com>
2024-07-28 16:39:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt()+1;
-		for(int i=1; i<input ;i++){
-            System.out.println("*".repeat(i));
-        }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int line = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i < line+1; i++) {
+			System.out.println("*".repeat(i));
+		}
 	}
 }
