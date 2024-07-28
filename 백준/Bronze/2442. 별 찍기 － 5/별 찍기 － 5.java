975a6b0991848a9b28247c6549a5d2def4d0fff6
eosa1414 <eosa1414@gmail.com>
2024-07-28 17:09:53 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 136 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));){
+			
+		int line = Integer.parseInt(br.readLine());
+			
+		for(int i = 1; i <2 * line; i += 2) {
+			System.out.print(" ".repeat((line*2 - i)/2));
+			System.out.print("*".repeat(i));
+			System.out.println();
+		}
+		
+		}catch(NumberFormatException | IOException e) {
+			System.out.println("예외 발생. 입력값이 잘못되었습니다.");
+		}
+		
+	}
+}
