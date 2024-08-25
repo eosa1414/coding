2695d28c75e8bcd834317b1f01cd8a3a1b168814
eosa1414 <eosa1414@gmail.com>
2024-08-25 22:25:05 +0900

    Update 색종이 － 2.java

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -4,45 +4,70 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        boolean[][] paper = new boolean[101][101];
-        
-        // 색종이 붙이기
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            
-            for (int r = x; r < x + 10; r++) {
-                for (int c = y; c < y + 10; c++) {
-                    paper[r][c] = true;
-                }
-            }
-        }
-        
-        // 둘레 길이 계산
-        int[] dr = {0, 1, 0, -1}; // 우하좌상
-        int[] dc = {1, 0, -1, 0};
-        int perimeter = 0;
-        
-        for (int r = 0; r < 101; r++) {
-            for (int c = 0; c < 101; c++) {
-                if (paper[r][c]) {
-                    for (int d = 0; d < 4; d++) {
-                        int nr = r + dr[d];
-                        int nc = c + dc[d];
-                        
-                        if (nr < 0 || nr >= 101 || nc < 0 || nc >= 101 || !paper[nr][nc]) {
-                            perimeter++;
+	static final short DOUBLE_BORDER = 4;
+	static final short BORDER = 2;
+	static final short INNER = 1;
+	static final short NOTHING = 0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		short[][] paper = new short[101][101];
+		int minR = 100;
+		int minC = 100;
+		int maxR = 0;
+		int maxC = 0;
+		for(int i = 0; i < N; i++) {//네 번 반복
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			for(int c = y; c <= y + 9; c++) {
+				if(paper[x][c] == NOTHING) {
+					paper[x][c] = BORDER;
+				}
+			}
+			for(int r = x + 1; r <= x + 8; r++) {
+				if(paper[r][y] == NOTHING) {
+					paper[r][y] = BORDER;
+				}
+				for(int c = y + 1; c <= y + 8; c++) {
+					paper[r][c] = INNER;
+				}
+				if(paper[r][y + 9] == NOTHING) {
+					paper[r][y + 9] = BORDER;
+				}
+			}
+			for(int c = y; c <= y + 9; c++) {
+				if(paper[x + 9][c] == NOTHING) {
+					paper[x + 9][c] = BORDER;
+				}
+			}
+			minR = Math.min(minR, x);
+			maxR = Math.max(maxR, x + 9);
+			minC = Math.min(minC, y);
+			maxC = Math.max(maxC, y + 9);
+		}
+		
+		//우하좌상
+		int[] dr = {0, 1, 0, -1};
+		int[] dc = {1, 0, -1, 0};
+		int cnt = 0;
+		for(int r = minR; r <= maxR; r++) {
+			for(int c = minC; c <= maxC; c++) {
+				if(paper[r][c] == BORDER) {
+					for (int i = 0; i < 4; i++) {
+                        int nr = r + dr[i];
+                        int nc = c + dc[i];
+                        if (nr < minR || nr > maxR || nc < minC || nc > maxC || paper[nr][nc] == NOTHING) {
+                            cnt++;
                         }
                     }
-                }
-            }
-        }
-        
-        System.out.println(perimeter);
-    }
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
 }
